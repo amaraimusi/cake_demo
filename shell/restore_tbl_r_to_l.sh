53cf880b598ebc39c0a70e991103fa86ac57bfd3
k_uehara <amaraimusi@gmail.com>
2020-02-06 05:42:58 +0900

    shell テーブルのrestore

---
+++ shell/restore_tbl_r_to_l.sh
@@ -0,0 +1,12 @@
+﻿#!/bin/bash
+#read pw
+pw="aka3siro3"
+echo "A1"
+test1="hello_world"
+
+ssh -l amaraimusi amaraimusi.sakura.ne.jp "
+	sh www/cake_demo/shell/server/test.sh aka3siro3;
+	"
+
+echo "出力完了"
+cmd /k
