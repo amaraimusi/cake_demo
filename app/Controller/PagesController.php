a7e0f4a05781c0a9e6952236cf8c6168f743d5d6
user <user@host.docker.internal>
2021-06-14 12:06:48 +0900

    Pagesの整備

--- app/Controller/PagesController.php
+++ app/Controller/PagesController.php
@@ -2,23 +2,49 @@
 App::uses('AppController', 'Controller');
 
 /**
- * 管理者トップ
+ * Pages
  * 
- * @since 2021-6-11
+ * @version 3.0
+ * @since 2014-8-21 | 2021-6-14
+ * @author k-uehara
  *
  */
 class PagesController extends AppController {
 	public $name = 'Pages';
 	public $uses = false;
-	public $components=null;//ログイン認証不要
+	//public $components=null;//ログイン認証不要
 	public $logout_flg=false;//ログアウトリンクを非表示
+	
+	public function beforeFilter() {
+		
+		// 未ログイン中である場合、未認証モードの扱いでページ表示する。
+		if($this->login_flg == 0 && empty($this->Auth->user())){
+			$this->Auth->allow(); // 未認証モードとしてページ表示を許可する。
+		}
+		
+		parent::beforeFilter();
+		
+	}
 
-    public function index() {
-    	$this->autoRender = false;//ビューを使わない。
-    	$home_url = $this->webroot . '?a=1';
-    	echo 'Logout.<br>';
-    	echo "<a href='{$home_url}'>To system home.</a>";
-    }
+	public function index() {
+		//$userInfo = $this->Auth->user();
+		$userInfo = $this->getUserInfo();
+		
+		
+		$err = '';
+		if(empty($userInfo['id'])){
+			$err = 'ログインされてません。';
+		}else{
+			
+			$role_level = $userInfo['authority']['level'];
+			if($role_level < 0){
+				$err = 'ログインできません。';
+			}
+		}
+		
+		$this->set(['err'=>$err]);
+		
+	}
 
 
 }
